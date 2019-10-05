60d4f2268a5e3191c6416b7cb4a3b7f25e9c06a3
Daniel Jaimes <daniel.jaimes.2015@upb.edu.co>
2019-10-05 14:01:53 -0500

    Estructura CRUD + Login terminada

--- src/main/java/edu/upb/laboratorioejb/controller/UsuarioController.java
+++ src/main/java/edu/upb/laboratorioejb/controller/UsuarioController.java
@@ -10,8 +10,10 @@ import edu.upb.laboratorioejb.model.Usuario;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.ResourceBundle;
 import javax.ejb.EJB;
 import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 
 /**
  *
@@ -22,15 +24,16 @@ import javax.faces.model.DataModel;
 public class UsuarioController implements Serializable {
 
     private Usuario actual;
-    private DataModel listado = null;
+    private DataModel<Usuario> listado = null;
 
     @EJB
     UsuarioDAO ejbDAO;
+    private Object JsfUtil;
 
     public UsuarioController() {
         actual = new Usuario();
     }
-    
+
     public Usuario getActual() {
         return actual;
     }
@@ -38,21 +41,57 @@ public class UsuarioController implements Serializable {
     public void setActual(Usuario actual) {
         this.actual = actual;
     }
-    
-    
-    public String login(){
-        
-        Usuario result =ejbDAO.findByAuth(actual);
-        
-        if(result == null){
+
+    public String login() {
+
+        Usuario result = ejbDAO.findByAuth(actual);
+
+        if (result == null) {
             actual = new Usuario();
             return "index?faces-redirect=true";
-        }else{
+        } else {
             actual = result;
             return "home?faces-redirect=true";
         }
     }
 
-    
-    
+    public DataModel<Usuario> getListado() {
+        if (listado == null) {
+            listado = new ListDataModel<Usuario>(ejbDAO.findAll());
+        }
+        return listado;
+    }
+
+    public String prepareEdit() {
+        actual = listado.getRowData();
+        return "edit?faces-redirect=true";
+    }
+
+    public String edit() {
+        ejbDAO.edit(actual);
+        return "userlist?faces-redirect=true";
+    }
+
+    public String prepareList() {
+        listado = null;
+        return "userlist?faces-redirect=true";
+    }
+
+    public String prepareCreate() {
+        actual = new Usuario();
+        return "create?faces-redirect=true";
+    }
+
+    public String create() {
+        ejbDAO.create(actual);
+        prepareList();
+        return "userlist?faces-redirect=true";
+    }
+
+    public String delete() {
+        actual = listado.getRowData();
+        ejbDAO.remove(actual);
+        prepareList();
+        return "userlist?faces-redirect=true";
+    }
 }
